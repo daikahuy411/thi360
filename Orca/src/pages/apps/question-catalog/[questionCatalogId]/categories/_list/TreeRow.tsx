5365d2fb3594cbf3a88dc243e5f0e063ce3ef99a
Minh Tri Nguyen <tri.nguyen@centerbase.com>
2023-06-15 00:49:54 -0600

    [question-category]
    - crud question-category
    - add package.json for datepickers

--- Orca/src/pages/apps/question-catalog/[questionCatalogId]/categories/_list/TreeRow.tsx
+++ Orca/src/pages/apps/question-catalog/[questionCatalogId]/categories/_list/TreeRow.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react'
 
+import { QuestionCategoryApi } from 'api/catalog-api'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import Draggable from 'react-draggable'
+import toast from 'react-hot-toast'
 
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import DeleteIcon from '@mui/icons-material/Delete'
 import DescriptionIcon from '@mui/icons-material/Description'
 import EditIcon from '@mui/icons-material/Edit'
 import Folder from '@mui/icons-material/Folder'
 import FolderOpenIcon from '@mui/icons-material/FolderOpen'
-import Checkbox from '@mui/material/Checkbox'
+import { Button } from '@mui/material'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
 import IconButton from '@mui/material/IconButton'
+import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack'
 import TableCell from '@mui/material/TableCell'
 import TableRow from '@mui/material/TableRow'
@@ -19,6 +29,17 @@ type TreeRowProps = {
   item: any
   excludedId: number
   level: number
+}
+
+function PaperComponent(props) {
+  return (
+    <Draggable
+      handle="#draggable-dialog-title"
+      cancel={'[class*="MuiDialogContent-root"]'}
+    >
+      <Paper {...props} />
+    </Draggable>
+  );
 }
 
 export default function TreeRow(props: TreeRowProps) {
@@ -44,12 +65,33 @@ export default function TreeRow(props: TreeRowProps) {
       )
     })
 
+/*
+  * handle remove question-catalog
+  */
+const [openDelete, setOpenDelete] = useState(false);
+const handleClickOpenDelete = () => setOpenDelete(true);
+const handleCloseDelete = () => setOpenDelete(false);
+const handleDelete = (item) => {
+  if (item) {
+    QuestionCategoryApi.delete({ id: Number(item.key) })
+      .then((response) => {
+        setOpenDelete(false)
+        toast.success('Xóa dữ liệu thành công.')
+        setTimeout(() => router.reload(), 1000)
+      })
+      .catch((e) => {
+        setOpenDelete(false)
+        toast.error('Xảy ra lỗi trong quá trình xóa dữ liệu. Vui lòng thử lại sau!')
+      })
+  }
+}
+/*
+* handle remove question-catalog
+*/
+
   return (
     <>
-      <TableRow onClick={() => setIsCollapsed(!isCollapsed)}>
-        <TableCell padding='checkbox'>
-          <Checkbox />
-        </TableCell>
+      <TableRow key={`${item.key}-row`} onClick={() => setIsCollapsed(!isCollapsed)}>
         <TableCell component='th' scope='row'>
           <IconButton
             aria-label='filter'
@@ -57,6 +99,9 @@ export default function TreeRow(props: TreeRowProps) {
             href={`/apps/question-catalog/${questionCatalogId}/categories/${item.key}`}
           >
             <EditIcon />
+          </IconButton>
+          <IconButton aria-label='filter' onClick={() => handleClickOpenDelete()}>
+            <DeleteIcon />
           </IconButton>
         </TableCell>
         <TableCell component='th' scope='row'>
@@ -73,6 +118,26 @@ export default function TreeRow(props: TreeRowProps) {
         <TableCell align='right'>{item.totalUser}</TableCell>
       </TableRow>
       {!isCollapsed && <>{children}</>}
+
+      <Dialog
+              open={openDelete}
+              onClose={handleCloseDelete}
+              PaperComponent={PaperComponent}
+              aria-labelledby="draggable-dialog-title"
+            >
+              <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+                Xác nhận
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Dữ liệu xóa sẽ không thể khôi phục lại. Bạn có muốn xóa Danh mục câu hỏi này không?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button autoFocus onClick={handleCloseDelete}> Hủy bỏ </Button>
+                <Button onClick={() => handleDelete(item)} color='error'>Đồng ý</Button>
+              </DialogActions>
+            </Dialog>
     </>
   )
 }
--- Orca/src/pages/apps/question-catalog/[questionCatalogId]/categories/_list/index.js
+++ Orca/src/pages/apps/question-catalog/[questionCatalogId]/categories/_list/index.js
@@ -10,7 +10,6 @@ import { useRouter } from 'next/router'
 import Icon from '@core/components/icon'
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined'
 import Button from '@mui/material/Button'
-import Checkbox from '@mui/material/Checkbox'
 import Divider from '@mui/material/Divider'
 import Grid from '@mui/material/Grid'
 import IconButton from '@mui/material/IconButton'
@@ -33,6 +32,8 @@ const CategoryTable = () => {
   const router = useRouter()
   const [data, setData] = useState([])
   const [page, setPage] = useState(0)
+  const [totalItem, setTotalItem] = useState(0)
+  const [totalParentItem, setTotalParentItem] = useState(0)
   const [rowsPerPage, setRowsPerPage] = useState(10)
   const { questionCatalogId } = router.query
 
@@ -47,20 +48,32 @@ const CategoryTable = () => {
 
   const fetchData = () => {
     if (!questionCatalogId || questionCatalogId == 0) return
-    QuestionCategoryApi.getCategoriesByCatalogId(questionCatalogId).then(response => {
-      setData(response.data)
-    })
+
+    const param = {
+      keyword: '',
+      catalogId: Number(questionCatalogId),
+      page: page == 0 ? 1 : page + 1,
+      limit: rowsPerPage
+    }
+
+    QuestionCategoryApi.searches(param)
+      .then(response => {
+        setData(response.data.value)
+        setTotalItem(response.data.totalItems)
+        setTotalParentItem(response.data.totalParentItems)
+      })
+      .catch((e) => console.log(e))
   }
 
   useEffect(() => {
     fetchData()
-  }, [questionCatalogId])
+  }, [questionCatalogId, page, rowsPerPage ])
 
   return (
     <>
       <Toolbar style={{ padding: 0 }}>
         <Typography sx={{ flex: '1 1 100%' }} variant='h5' id='tableTitle' component='div'>
-          {data.length} Danh mục Câu hỏi
+          {totalItem} Danh mục Câu hỏi
         </Typography>
         &nbsp; &nbsp;
         <Tooltip title='Import'>
@@ -74,12 +87,12 @@ const CategoryTable = () => {
             <Icon icon='mdi:download' />
           </IconButton>
         </Tooltip>
-        &nbsp; &nbsp;
+        {/* &nbsp; &nbsp;
         <Tooltip title='Delete'>
           <IconButton sx={{ color: 'text.secondary' }}>
             <Icon icon='mdi:delete-outline' />
           </IconButton>
-        </Tooltip>
+        </Tooltip> */}
         &nbsp; &nbsp;
         <Button
           component={Link}
@@ -104,9 +117,10 @@ const CategoryTable = () => {
         </Grid>
         <Grid item md={6} alignContent={'right'}>
           <TablePagination
+            labelRowsPerPage='Số dòng/trang'
             rowsPerPageOptions={[10, 25, 100]}
             component='div'
-            count={10}
+            count={totalParentItem}
             rowsPerPage={rowsPerPage}
             page={page}
             onPageChange={handleChangePage}
@@ -118,15 +132,7 @@ const CategoryTable = () => {
         <Table sx={{ minWidth: 650 }} aria-label='simple table'>
           <TableHead>
             <TableRow>
-              <TableCell padding='checkbox'>
-                <Checkbox
-                  // onChange={onSelectAllClick}
-                  // checked={rowCount > 0 && numSelected === rowCount}
-                  inputProps={{ 'aria-label': 'select all desserts' }}
-                  // indeterminate={numSelected > 0 && numSelected < rowCount}
-                />
-              </TableCell>
-              <TableCell style={{ width: 30 }}>Sửa</TableCell>
+              <TableCell style={{ width: 120 }}>#</TableCell>
               <TableCell>Tên</TableCell>
               <TableCell align='right' style={{ width: 120 }}>
                 Số câu hỏi
@@ -134,14 +140,15 @@ const CategoryTable = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {data && data.map(item => <TreeRow key={item.Id} item={item} excludedId={0} nodeId={item.Id} level={0} />)}
+            {data && data.map((item, index) => <TreeRow key={index} item={item} excludedId={0} nodeId={item.Id} level={0} />)}
           </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
+        labelRowsPerPage='Số dòng/trang'
         rowsPerPageOptions={[10, 25, 100]}
         component='div'
-        count={10}
+        count={totalParentItem}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
