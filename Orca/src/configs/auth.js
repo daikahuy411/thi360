cc5609502132de0033d3e31edf0e18086d871832
cuvu <vucuongkg@gmail.com>
2023-07-03 21:18:53 +0700

    Revert config

--- Orca/src/configs/auth.js
+++ Orca/src/configs/auth.js
@@ -2,11 +2,8 @@ export default {
   // baserUrl: 'http://localhost:7043/',
   // baseApiUrl: 'http://localhost:7043/api/',
 
-  baserUrl: 'http://localhost:8888/',
-  baseApiUrl: 'http://localhost:8888/api/',
-
-  // baserUrl: 'http://beta.thi360.com',
-  // baseApiUrl: 'http://beta.thi360.com/api/',
+  baserUrl: 'http://beta.thi360.com',
+  baseApiUrl: 'http://beta.thi360.com/api/',
 
   meEndpoint: '/auth/me',
   loginEndpoint: 'users/authenticate',
