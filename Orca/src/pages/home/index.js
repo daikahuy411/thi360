c3bc106401850f41973a435bd21dfb0251edc0fc
cuvu <vucuongkg@gmail.com>
2023-10-29 15:56:15 +0700

    Improve Home page

--- Orca/src/pages/home/index.js
+++ Orca/src/pages/home/index.js
@@ -43,7 +43,9 @@ const HomePage = () => {
           <h1 className='' style={{ color: 'white', fontSize: 36 }}>
             Thi360 Testing online
           </h1>
-          <h2 class="" style={{ color: 'white', fontSize: 20 }}>The Most Comprehensive Preparation App for All Exams </h2>
+          <h2 class='' style={{ color: 'white', fontSize: 20 }}>
+            The Most Comprehensive Preparation App for All Exams{' '}
+          </h2>
           {/* <p>Thực hiện các bài kiểm tra thử của chúng tôi bao nhiêu tùy thích MIỄN PHÍ!</p> */}
           {/* <button type='button' className='btn btn-outline-primary btn-sm waves-effect text-white'>
             Thi thử
@@ -136,8 +138,122 @@ const HomePage = () => {
             </a>
           </div>
         </section> */}
-
-        <section className='Home-why'>
+        <br />
+        <section className="mb5-l mb4-5 fixed-width-container ph3 ph0-l">
+          <div className="flex flex-column w-100">
+            <h3 className="home_head__DVePE tc mb3 mt0">Start your preparation</h3>
+            <div className="home-section-container">
+              <div className="home-section-item" style={{ backgroundImage: "url('https://gs-post-images.grdp.co/2021/3/group-38-2x-img1614867563918-41.png-rs-high-webp.png')" }}>
+                <span className="white fw7 f4-l f5">Online Classroom Program</span>
+                <span className="white mb4 f6 w-70 h2-5">Live &amp; Recorded courses by Top Faculty</span>
+                <a className="mt-auto link" href="/">
+                  <span className="secondary mr2 f5 f6-l">Explore Courses</span>
+                  <svg version="1.1" width="8" height="8" className="svg-f-secondary" viewBox="0 0 30 48" preserveAspectRatio="" style={{ strokeWwidth: 1 }}>
+                    <path d="M0 4.7C6.4 11.1 12.8 17.5 19.4 24 12.9 30.4 6.5 36.7 0.1 43.1c1.7 1.7 3.3 3.3 4.7 4.7 8-8 16.1-16 24.2-24 -8.1-8-16.1-16-24.2-24C3.4 1.3 1.8 2.9 0 4.7z"></path>
+                    <path d="M0 4.7c1.8-1.8 3.4-3.4 4.9-4.9 8 8 16.1 16 24.2 24 -8.1 8-16.1 16-24.2 24 -1.5-1.5-3.1-3.1-4.7-4.7C6.5 36.7 12.9 30.4 19.4 24 12.8 17.5 6.4 11.1 0 4.7z"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="home-section-item" style={{ backgroundImage: "url('https://gs-post-images.grdp.co/2021/3/group-38-2x-img1614867563918-41.png-rs-high-webp.png')" }}>
+                <span className="white fw7 f4-l f5">Test Series</span>
+                <span className="white mb4 f6 w-70 h2-5">Practice unlimited mock tests and get your All India Rank</span>
+                <a className="mt-auto link" href="/">
+                  <span className="secondary mr2 f5 f6-l">Explore Test Series</span>
+                  <svg version="1.1" width="8" height="8" className="svg-f-secondary" viewBox="0 0 30 48" preserveAspectRatio="" style={{ strokeWwidth: 1 }}>
+                    <path d="M0 4.7C6.4 11.1 12.8 17.5 19.4 24 12.9 30.4 6.5 36.7 0.1 43.1c1.7 1.7 3.3 3.3 4.7 4.7 8-8 16.1-16 24.2-24 -8.1-8-16.1-16-24.2-24C3.4 1.3 1.8 2.9 0 4.7z"></path>
+                    <path d="M0 4.7c1.8-1.8 3.4-3.4 4.9-4.9 8 8 16.1 16 24.2 24 -8.1 8-16.1 16-24.2 24 -1.5-1.5-3.1-3.1-4.7-4.7C6.5 36.7 12.9 30.4 19.4 24 12.8 17.5 6.4 11.1 0 4.7z"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="home-section-item" style={{ backgroundImage: "url('https://gs-post-images.grdp.co/2021/3/group-38-2x-img1614867563918-41.png-rs-high-webp.png')" }}>
+                <span className="white fw7 f4-l f5">Free Videos</span>
+                <span className="white mb4 f6 w-70 h2-5">High Quality Content for Complete Conceptual Clarity</span>
+                <a className="mt-auto link" href="/">
+                  <span className="secondary mr2 f5 f6-l">Explore Free Videos</span>
+                  <svg version="1.1" width="8" height="8" className="svg-f-secondary" viewBox="0 0 30 48" preserveAspectRatio="" style={{ strokeWwidth: 1 }}>
+                    <path d="M0 4.7C6.4 11.1 12.8 17.5 19.4 24 12.9 30.4 6.5 36.7 0.1 43.1c1.7 1.7 3.3 3.3 4.7 4.7 8-8 16.1-16 24.2-24 -8.1-8-16.1-16-24.2-24C3.4 1.3 1.8 2.9 0 4.7z"></path>
+                    <path d="M0 4.7c1.8-1.8 3.4-3.4 4.9-4.9 8 8 16.1 16 24.2 24 -8.1 8-16.1 16-24.2 24 -1.5-1.5-3.1-3.1-4.7-4.7C6.5 36.7 12.9 30.4 19.4 24 12.8 17.5 6.4 11.1 0 4.7z"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="home-section-item" style={{ backgroundImage: "url('https://gs-post-images.grdp.co/2021/3/group-38-2x-img1614867563918-41.png-rs-high-webp.png')" }}>
+                <span className="white fw7 f4-l f5">Previous Year Papers</span>
+                <span className="white mb4 f6 w-70 h2-5">Practice past years' question papers and get exam ready</span>
+                <a className="mt-auto link" href="/">
+                  <span className="secondary mr2 f5 f6-l">Explore Papers</span>
+                  <svg version="1.1" width="8" height="8" className="svg-f-secondary" viewBox="0 0 30 48" preserveAspectRatio="" style={{ strokeWwidth: 1 }}>
+                    <path d="M0 4.7C6.4 11.1 12.8 17.5 19.4 24 12.9 30.4 6.5 36.7 0.1 43.1c1.7 1.7 3.3 3.3 4.7 4.7 8-8 16.1-16 24.2-24 -8.1-8-16.1-16-24.2-24C3.4 1.3 1.8 2.9 0 4.7z"></path>
+                    <path d="M0 4.7c1.8-1.8 3.4-3.4 4.9-4.9 8 8 16.1 16 24.2 24 -8.1 8-16.1 16-24.2 24 -1.5-1.5-3.1-3.1-4.7-4.7C6.5 36.7 12.9 30.4 19.4 24 12.8 17.5 6.4 11.1 0 4.7z"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="home-section-item" style={{ backgroundImage: "url('https://gs-post-images.grdp.co/2021/3/group-38-2x-img1614867563918-41.png-rs-high-webp.png')" }}>
+                <span className="white fw7 f4-l f5">Free Quizzes</span>
+                <span className="white mb4 f6 w-70 h2-5">Attempt topic wise quizzes for improving your score</span>
+                <a className="mt-auto link" href="/">
+                  <span className="secondary mr2 f5 f6-l">Explore Quizzes</span>
+                  <svg version="1.1" width="8" height="8" className="svg-f-secondary" viewBox="0 0 30 48" preserveAspectRatio="" style={{ strokeWwidth: 1 }}>
+                    <path d="M0 4.7C6.4 11.1 12.8 17.5 19.4 24 12.9 30.4 6.5 36.7 0.1 43.1c1.7 1.7 3.3 3.3 4.7 4.7 8-8 16.1-16 24.2-24 -8.1-8-16.1-16-24.2-24C3.4 1.3 1.8 2.9 0 4.7z"></path>
+                    <path d="M0 4.7c1.8-1.8 3.4-3.4 4.9-4.9 8 8 16.1 16 24.2 24 -8.1 8-16.1 16-24.2 24 -1.5-1.5-3.1-3.1-4.7-4.7C6.5 36.7 12.9 30.4 19.4 24 12.8 17.5 6.4 11.1 0 4.7z"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <br />
+        <section className='mb5-l mb4-5 fixed-width-container ph3 ph0-l'>
+          <div className='flex flex-column w-100'>
+            <div className='ba-title'>
+              <h2> Tại sao nên sử dụng Thi360.com? </h2>
+            </div>
+            <div className='flex justify-between'>
+              <div className='flex flex-column items-center w-30'>
+                <img
+                  src='https://grdp.co/cdn-cgi/image/quality=100,width=200,f=auto/https://gs-post-images.grdp.co/2021/8/group-6-2x-img1629283053211-98.png-rs-high-webp.png'
+                  className='w4-2-l h4-l w3 h2-5 mb3 '
+                  alt='Online Classroom Program'
+                />
+                <p
+                  className='mv0 primary f6 f3-l fw7-l fw5 tc'
+                  style={{ color: 'rgb(129, 53, 136)', fontWeight: 'bold', fontSize: '1.5rem' }}
+                >
+                  Online Classroom Program
+                </p>
+                <p className='mv0 f5 fw7 tc dn db-l'>by Top Faculty</p>
+              </div>
+              <div className='flex flex-column items-center w-30'>
+                <img
+                  src='https://grdp.co/cdn-cgi/image/quality=100,width=200,f=auto/https://gs-post-images.grdp.co/2021/8/illustrations-live-courses-2x-img1629272458441-51.png-rs-high-webp.png'
+                  className='w4-2-l h4-l w3 h2-5 mb3 mh2 mh4-l'
+                  alt='Comprehensive Study Material'
+                />
+                <p
+                  className='mv0 primary f6 f3-l fw7-l fw5 tc'
+                  style={{ color: 'rgb(129, 53, 136)', fontWeight: 'bold', fontSize: '1.5rem' }}
+                >
+                  Comprehensive Study Material
+                </p>
+                <p className='mv0 f5 fw7 tc dn db-l'>for All Exams</p>
+              </div>
+              <div className='flex flex-column items-center w-30'>
+                <img
+                  src='https://grdp.co/cdn-cgi/image/quality=100,width=200,f=auto/https://gs-post-images.grdp.co/2021/8/group-19-woman-mask-2-2x-img1629283123665-66.png-rs-high-webp.png'
+                  className='w4-2-l h4-l w3 h2-5 mb3 '
+                  alt='Latest Pattern Test Series'
+                />
+                <p
+                  className='mv0 primary f6 f3-l fw7-l fw5 tc'
+                  style={{ color: 'rgb(129, 53, 136)', fontWeight: 'bold', fontSize: '1.5rem' }}
+                >
+                  Latest Pattern Test Series
+                </p>
+                <p className='mv0 f5 fw7 tc dn db-l'>with Detailed Analysis</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* <section className='Home-why'>
           <div className='ba-title'>
             <h2> Tại sao nên sử dụng Thi360.com? </h2>
           </div>
@@ -145,16 +261,13 @@ const HomePage = () => {
             <div className='row g-2'>
               <div className='col-md-12 col-sm-12'>
                 <br />
-                <img src='/images/home/Screenshot_316.png' style={{ width: '100%' }} />
-                <br />
                 <img src='/images/home/Screenshot_315.png' style={{ width: '100%' }} />
                 <br />
               </div>
             </div>
           </div>
-        </section>
-
-        <section className='Home-why'>
+        </section> */}
+        {/* <section className='Home-why'>
           <div className='ba-title'>
             <h2> Tại sao nên sử dụng Thi360.com? </h2>
           </div>
@@ -210,7 +323,7 @@ const HomePage = () => {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
         <br />
         <section className='Home-num1'>
           <div className='ba-title'>
